7a45a492ee0d660ba08c522d56d37c9f56e8f864
Ayush2927 <ayushm050405@gmail.com>
2026-04-07 19:59:21 +0530

    Refactor database connection to handle index dropping for attendances collection

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,16 @@ import mongoose from "mongoose"
 
 const connectDB=async()=>{
    try {
-     await mongoose.connect(process.env.MONGO_URI);
+     const conn = await mongoose.connect(process.env.MONGO_URI);
      console.log("Mongo DB connected");
+     
+     try {
+       await conn.connection.db.collection('attendances').dropIndex('user_1_date_1');
+       console.log("Dropped problematic index user_1_date_1");
+     } catch (idxError) {
+       // Ignore if index doesn't exist
+       console.log("Index user_1_date_1 might not exist or already dropped");
+     }
  
    } catch (error) {
     console.log(error);
